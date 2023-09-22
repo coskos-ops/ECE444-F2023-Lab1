28ceaba9d850a51df624b742168a82da2dd30597
coskos-ops <afilkostic@gmail.com>
2023-09-22 15:33:45 -0400

    Completed extra functionality for Activity 4

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'nonsense'
@@ -13,8 +13,9 @@ moment = Moment(app)
 
 
 
-class NameForm(FlaskForm):
+class NameEmailForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 
@@ -23,14 +24,18 @@ class NameForm(FlaskForm):
 from flask import Flask, render_template, session, redirect, url_for, flash
 @app.route('/', methods=['GET', 'POST'])
 def index():
-     form = NameForm()
+     form = NameEmailForm()
      if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-     return render_template('index.html', form=form, name=session.get('name'))
+     return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 
 
